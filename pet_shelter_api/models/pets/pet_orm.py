8ef9b7240e725a7502f7d299a230eb36caf95d7b
David-Lor <17401854+david-lor@users.noreply.github.com>
2020-12-26 17:51:58 +0100

    Fix primary key definition on Pet ORM model

--- pet_shelter_api/models/pets/pet_orm.py
+++ pet_shelter_api/models/pets/pet_orm.py
@@ -7,6 +7,6 @@ from ...helpers import get_uuid
 class PetORM(ORMBase):
     __tablename__ = "pets"
 
-    pet_id = Column(UUID, index=True, unique=True, nullable=False, default=get_uuid)
+    pet_id = Column(UUID, primary_key=True, default=get_uuid)
     name = Column(STRING, nullable=False)
     animal_type = Column(STRING, nullable=False)
